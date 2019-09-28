f886e993bd9aa327ae3f411be49cdde9df4b4b2a
Andy Williams <andy@andy.xyz>
2019-09-28 19:29:03 +0100

    Update to latest theme features

--- theme.go
+++ theme.go
@@ -17,12 +17,32 @@ func (beebTheme) ButtonColor() color.Color {
 	return color.White
 }
 
+func (beebTheme) DisabledButtonColor() color.Color {
+	return color.White
+}
+
+func (beebTheme) IconColor() color.Color {
+	return color.White
+}
+
+func (beebTheme) DisabledIconColor() color.Color {
+	return color.Black
+}
+
 func (beebTheme) HyperlinkColor() color.Color {
 	return color.White
 }
 
 func (beebTheme) TextColor() color.Color {
 	return color.White
+}
+
+func (beebTheme) DisabledTextColor() color.Color {
+	return color.White
+}
+
+func (beebTheme) HoverColor() color.Color {
+	return color.Black
 }
 
 func (beebTheme) PlaceHolderColor() color.Color {
@@ -38,6 +58,10 @@ func (beebTheme) FocusColor() color.Color {
 }
 
 func (beebTheme) ScrollBarColor() color.Color {
+	return color.Black
+}
+
+func (beebTheme) ShadowColor() color.Color {
 	return color.Black
 }
 
@@ -74,5 +98,9 @@ func (beebTheme) IconInlineSize() int {
 }
 
 func (beebTheme) ScrollBarSize() int {
+	return 10
+}
+
+func (beebTheme) ScrollBarSmallSize() int {
 	return 10
 }
